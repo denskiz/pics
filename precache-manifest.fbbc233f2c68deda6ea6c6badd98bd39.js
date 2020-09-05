9e00e40bc5da7f141f5696a06501712194b0d24d
dennis <dennisent@protonmail.com>
2020-09-05 12:13:15 +1000

    Updates

---
+++ precache-manifest.fbbc233f2c68deda6ea6c6badd98bd39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aabff5f9ca32e3c09267b7683a706e1",
+    "url": "/pics/index.html"
+  },
+  {
+    "revision": "9f52b79f99747a81415a",
+    "url": "/pics/static/css/main.744c5823.chunk.css"
+  },
+  {
+    "revision": "83780c69b5c5574b9273",
+    "url": "/pics/static/js/2.e752a839.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pics/static/js/2.e752a839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f52b79f99747a81415a",
+    "url": "/pics/static/js/main.5c93e309.chunk.js"
+  },
+  {
+    "revision": "24445b424d0ed337bcc7",
+    "url": "/pics/static/js/runtime-main.db0de8af.js"
+  }
+]);
